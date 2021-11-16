f8202f06c4cc509203f5e6757fd9b2d81f385438
Roman Grinyov <w3lifer@gmail.com>
2021-11-17 01:23:08 +0300

    added copyTextToClipboard() function

--- js/popup.js
+++ js/popup.js
@@ -183,4 +183,14 @@ window.addEventListener('DOMContentLoaded', () => {
     return span;
   }
 
+  function copyTextToClipboard(text)
+  {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  }
+
 });
