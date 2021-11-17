aaa3a21335a1bbe31a57d6493cb2dfd8c170c7cb
Roman Grinyov <w3lifer@gmail.com>
2021-11-17 03:15:25 +0300

    preserve stored snippets

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,18 @@
 /* global chrome */
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({
-    snippets: {
-      char: [],
-      text: [],
-    },
+  chrome.storage.sync.get(storage => {
+    if (!storage.snippets) {
+      storage.snippets = {
+        char: [],
+        text: [],
+      };
+    }
+    chrome.storage.sync.set({
+      snippets: {
+        char: storage.snippets.char,
+        text: storage.snippets.text,
+      },
+    });
   });
 });
